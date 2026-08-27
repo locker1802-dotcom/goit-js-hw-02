b4bafe6732e1c9f29afba7326210b93b772a4a95
Dmytro Tsyukh <Locker1802@gmail.com>
2026-08-28 00:54:45 +0300

    v1

--- js/task-3.js
+++ js/task-3.js
@@ -1,9 +1,16 @@
-function getElementWidth(content, padding, border) {
-  const totalWidth = content + padding * 2 + border * 2;
+function checkForSpam(message) {
+  const lowerCaseMessage = message.toLowerCase();
 
-  return totalWidth;
+if (lowerCaseMessage.includes("spam") || lowerCaseMessage.includes("sale")) {
+  return true;
+}
+return false;
 }
 
-console.log(getElementWidth(50, 8, 4)); // 74
-console.log(getElementWidth(60, 12, 8.5)); // 101
-console.log(getElementWidth(200, 0, 0)); // 200
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
